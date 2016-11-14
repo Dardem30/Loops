716b5e2f493e2c0d1f28f0aa2fb91d70075a5b50
Dardem30 <r.lukashenko@mail.ru>
2016-11-14 19:57:45 +0300

    Initial commit

---
+++ src/Loops.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Роман on 14.11.2016.
+ */
+public class Loops {
+    public static void main(String[] args) {
+       int[] a;
+        for (int i=0;i<5;i++)
+            System.out.println ();
+}
+
+}
